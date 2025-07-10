cd930cef1aa595ed3d6a49ba1dd46af24ff1541c
Cathy Zhao <cathyjzhao@google.com>
2025-07-10 20:37:05 +0000

    added stencil code

---
+++ codelabs/grpc-rust-getting-started/start_here/src/client/client.rs
@@ -0,0 +1,19 @@
+use tonic::Request;
+
+use routeguide::route_guide_client::RouteGuideClient;
+use routeguide::{Point};
+
+pub mod routeguide {
+    tonic::include_proto!("routeguide");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    ///////////////////////////////////////////////////////////////////////////
+	// Codelab Hint: Logic for your gRPC Client will be added here.
+	//
+	// Steps include:
+	//  -   Create a connection to the gRPC server using RouteGuideClient::connect(...).
+	//  -   Call service methods on the client to interact with the server.
+	///////////////////////////////////////////////////////////////////////////
+}
