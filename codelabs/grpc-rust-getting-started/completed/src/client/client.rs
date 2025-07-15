744f007f4134bf4d704f8e93888b23cecc6c2610
Cathy Zhao <cathyjzhao@google.com>
2025-07-15 16:43:28 +0000

    remove unnecessary dependencies

--- codelabs/grpc-rust-getting-started/completed/src/client/client.rs
+++ codelabs/grpc-rust-getting-started/completed/src/client/client.rs
@@ -1,37 +1,38 @@
-use tonic::Request;
-use tonic::transport::{Channel, Endpoint}; 
+// use tonic::Request;
+// use tonic::transport::{Channel, Endpoint}; 
 
-pub mod route_guide_gen {
-    grpc::include_proto!("", "routeguide");
-}
+// pub mod route_guide_gen {
+//     grpc::include_proto!("", "routeguide");
+// }
 
-use route_guide_gen::{
-    route_guide_client::RouteGuideClient,
-    Point,
-};
+// use route_guide_gen::{
+//     route_guide_client::RouteGuideClient,
+//     Point,
+// };
 
-#[derive(Debug, Clone)]
-pub struct RouteGuideAppClient {
-    inner: RouteGuideClient<Channel>,
-}
+// #[derive(Debug, Clone)]
+// pub struct RouteGuideAppClient {
+//     inner: RouteGuideClient<Channel>,
+// }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    //Create endpoint to connect to
-    let endpoint = Endpoint::new("http://[::1]:10000")?; 
-    let channel = endpoint.connect().await?;             
+// #[tokio::main]
+// async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//     //Create endpoint to connect to
+//     let endpoint = Endpoint::new("http://[::1]:10000")?; 
+//     let channel = endpoint.connect().await?;             
 
-    // Create a new client
-    let mut client = RouteGuideClient::new(channel); 
+//     // Create a new client
+//     let mut client = RouteGuideClient::new(channel); 
 
-    println!("*** SIMPLE RPC ***");
-    let mut point = Point::new();
-    point.set_latitude(409_146_138); 
-    point.set_longitude(-746_188_906);
-    let response = client
-        .get_feature(Request::new(point))
-        .await?;
+//     println!("*** SIMPLE RPC ***");
+//     let mut point = Point::new();
+//     point.set_latitude(409_146_138); 
+//     point.set_longitude(-746_188_906);
+//     let response = client
+//         .get_feature(Request::new(point))
+//         .await?;
     
-    println!("RESPONSE = {response:?}");
-    Ok(())
-}
+//     println!("RESPONSE = {response:?}");
+//     Ok(())
+// }
+fn main(){}
--- codelabs/grpc-rust-getting-started/completed/target/debug/build/getting-started-6c8636cad361f73f/build_script_build-6c8636cad361f73f.d
+++
@@ -1,5 +0,0 @@
-/usr/local/google/home/cathyjzhao/grpc-codelabs/codelabs/grpc-rust-getting-started/completed/target/debug/build/getting-started-6c8636cad361f73f/build_script_build-6c8636cad361f73f.d: build.rs
-
-/usr/local/google/home/cathyjzhao/grpc-codelabs/codelabs/grpc-rust-getting-started/completed/target/debug/build/getting-started-6c8636cad361f73f/build_script_build-6c8636cad361f73f: build.rs
-
-build.rs:
