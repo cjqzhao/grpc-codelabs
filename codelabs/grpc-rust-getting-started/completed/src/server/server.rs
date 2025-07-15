62c52cb4aaf4cedc159ec9d5cacf55fae0b62d03
Cathy Zhao <cathyjzhao@google.com>
2025-07-15 16:51:54 +0000

    uncommmented code

--- codelabs/grpc-rust-getting-started/completed/src/server/server.rs
+++ codelabs/grpc-rust-getting-started/completed/src/server/server.rs
@@ -1,43 +1,42 @@
-// use std::sync::Arc;
-// use tonic::transport::Server;
-// use tonic::{Request, Response, Status};
-// pub mod routeguide {
-//     tonic::include_proto!("routeguide");
-// }
+use std::sync::Arc;
+use tonic::transport::Server;
+use tonic::{Request, Response, Status};
+pub mod routeguide {
+    tonic::include_proto!("routeguide");
+}
 
-// use routeguide::route_guide_server::{RouteGuide, RouteGuideServer};
-// use routeguide::{Feature, Point};
+use routeguide::route_guide_server::{RouteGuide, RouteGuideServer};
+use routeguide::{Feature, Point};
 
-// mod data;
+mod data;
 
-// #[derive(Debug)]
-// pub struct RouteGuideService {
-//     features: Arc<Vec<Feature>>,
-// }
+#[derive(Debug)]
+pub struct RouteGuideService {
+    features: Arc<Vec<Feature>>,
+}
 
-// #[tonic::async_trait]
-// impl RouteGuide for RouteGuideService {
-//     async fn get_feature(&self, request: Request<Point>) -> Result<Response<Feature>, Status> {
-//         println!("GetFeature = {:?}", request);
-//         for feature in &self.features[..] {
-//             if feature.location.as_ref() == Some(request.get_ref()) {
-//                 return Ok(Response::new(feature.clone()));
-//             }
-//         }
-//         Ok(Response::new(Feature::default()))
-//     }
-// }
+#[tonic::async_trait]
+impl RouteGuide for RouteGuideService {
+    async fn get_feature(&self, request: Request<Point>) -> Result<Response<Feature>, Status> {
+        println!("GetFeature = {:?}", request);
+        for feature in &self.features[..] {
+            if feature.location.as_ref() == Some(request.get_ref()) {
+                return Ok(Response::new(feature.clone()));
+            }
+        }
+        Ok(Response::new(Feature::default()))
+    }
+}
 
-// #[tokio::main]
-// async fn main() -> Result<(), Box<dyn std::error::Error>> {
-//     let addr = "[::1]:10000".parse().unwrap();
-//     println!("RouteGuideServer listening on: {addr}");
-//     let route_guide = RouteGuideService {
-//         features: Arc::new(data::load()),
-//     };
-//     let svc = RouteGuideServer::new(route_guide);
-//     Server::builder().add_service(svc).serve(addr).await?;
-//     Ok(())
-// }
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:10000".parse().unwrap();
+    println!("RouteGuideServer listening on: {addr}");
+    let route_guide = RouteGuideService {
+        features: Arc::new(data::load()),
+    };
+    let svc = RouteGuideServer::new(route_guide);
+    Server::builder().add_service(svc).serve(addr).await?;
+    Ok(())
+}
 
-fn main(){}
--- codelabs/grpc-rust-getting-started/completed/target/debug/deps/routeguide_server-bed9541553083665.d
+++ codelabs/grpc-rust-getting-started/completed/target/debug/deps/routeguide_server-bed9541553083665.d
@@ -1,5 +1,10 @@
-/usr/local/google/home/cathyjzhao/grpc-codelabs/codelabs/grpc-rust-getting-started/completed/target/debug/deps/routeguide_server-bed9541553083665.d: src/server/server.rs
+/usr/local/google/home/cathyjzhao/grpc-codelabs/codelabs/grpc-rust-getting-started/completed/target/debug/deps/routeguide_server-bed9541553083665.d: src/server/server.rs src/server/data.rs /usr/local/google/home/cathyjzhao/grpc-codelabs/codelabs/grpc-rust-getting-started/completed/target/debug/build/getting-started-dbf119be1a226823/out/routeguide.rs
 
-/usr/local/google/home/cathyjzhao/grpc-codelabs/codelabs/grpc-rust-getting-started/completed/target/debug/deps/routeguide_server-bed9541553083665: src/server/server.rs
+/usr/local/google/home/cathyjzhao/grpc-codelabs/codelabs/grpc-rust-getting-started/completed/target/debug/deps/routeguide_server-bed9541553083665: src/server/server.rs src/server/data.rs /usr/local/google/home/cathyjzhao/grpc-codelabs/codelabs/grpc-rust-getting-started/completed/target/debug/build/getting-started-dbf119be1a226823/out/routeguide.rs
 
 src/server/server.rs:
+src/server/data.rs:
+/usr/local/google/home/cathyjzhao/grpc-codelabs/codelabs/grpc-rust-getting-started/completed/target/debug/build/getting-started-dbf119be1a226823/out/routeguide.rs:
+
+# env-dep:CARGO_MANIFEST_DIR=/usr/local/google/home/cathyjzhao/grpc-codelabs/codelabs/grpc-rust-getting-started/completed
+# env-dep:OUT_DIR=/usr/local/google/home/cathyjzhao/grpc-codelabs/codelabs/grpc-rust-getting-started/completed/target/debug/build/getting-started-dbf119be1a226823/out
